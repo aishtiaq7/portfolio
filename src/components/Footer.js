a4e1576c625db004f4c7062362961cdde1bf7187
Awshaf <ahmmadawshaf@gmail.com>
2023-10-13 02:54:22 -0700

    turn off footer parallax for lg screens of 1848px or larger

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,28 @@ import "../App.css";
 import { CgCopyright } from "react-icons/cg";
 import { IconContext } from "react-icons";
 import { useSelector } from "react-redux";
+// eslint-disable-next-line
+import { useState } from "react";
 
 const Footer = (props) => {
   const { screenWidth } = useSelector((state) => state.globalStates);
+
+  // const [safeLargeSize, setSafeLargeSize] = useState(false);
   // console.log("sc:\t", screenWidth);
 
   // console.log("props.offset\t\t", props.offset);
   const value = 0.17;
 
   const getParallaxProperty = () => {
+    console.log('screenWidth:', screenWidth);
+
+    if(screenWidth >= 1848){
+      console.log('warning, lg scr!!')
+      // setSafeLargeSize(true)
+      return 'none';
+    }
+
+
     if (props.offset >= 4175 && screenWidth > 600) { // for lg screens
       return `translateY(${(props.offset - 4175) * value}px)`;
     } else if (props.offset >= 4638 && screenWidth < 600) { // for phones
@@ -28,11 +41,8 @@ const Footer = (props) => {
       <div
         className="links"
         style={{
-          transform:
-            // props.offset >= 3800
-            //   ? `translateY(${(props.offset - 3800) * value}px)`
-            //   : "none",
-            getParallaxProperty(),
+          transform: getParallaxProperty(),
+          // paddingTop: screenWidth >= 
         }}
       >
         <div className="contactMe">
