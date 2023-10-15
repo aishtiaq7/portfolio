6875cd8bf6ba106c8da630d089bbba163fcaffea
aishtiaq <awshaf_ishtiaque@sfu.ca>
2023-10-14 18:18:12 -0700

    moved footer styles to its own file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,49 @@
-import "../App.css";
+
 import { CgCopyright } from "react-icons/cg";
 import { IconContext } from "react-icons";
-import { useSelector } from "react-redux";
 // eslint-disable-next-line
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+
+import "./Footer.css"
+function getWindowSize() {
+  const {innerWidth, innerHeight} = window;
+  return {innerWidth, innerHeight};
+}
+
 
 const Footer = (props) => {
-  const { screenWidth } = useSelector((state) => state.globalStates);
 
-  // const [safeLargeSize, setSafeLargeSize] = useState(false);
-  // console.log("sc:\t", screenWidth);
 
-  // console.log("props.offset\t\t", props.offset);
-  const value = 0.17;
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  console.log('new windowSize: ', windowSize.innerWidth);
+
+  const value = 0.117;
 
   const getParallaxProperty = () => {
-    console.log('screenWidth:', screenWidth);
 
-    if(screenWidth >= 1848){
+    if(windowSize.innerWidth >= 1848){
       console.log('warning, lg scr!!')
-      // setSafeLargeSize(true)
       return 'none';
     }
 
 
-    if (props.offset >= 4175 && screenWidth > 600) { // for lg screens
+    if (props.offset >= 4175 && windowSize.innerWidth > 600) { // for lg screens
       return `translateY(${(props.offset - 4175) * value}px)`;
-    } else if (props.offset >= 4638 && screenWidth < 600) { // for phones
+    } else if (props.offset >= 4638 && windowSize.innerWidth < 600) { // for phones
       return `translateY(${(props.offset - 4638) * value}px)`;
     } else {
       return `none`;
@@ -42,7 +59,6 @@ const Footer = (props) => {
         className="links"
         style={{
           transform: getParallaxProperty(),
-          // paddingTop: screenWidth >= 
         }}
       >
         <div className="contactMe">
