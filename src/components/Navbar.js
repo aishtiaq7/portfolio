ab66883188395b001765f2a12ceb1b758ddb1baf
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-08-27 03:23:00 -0700

    name text scramble effect working and setInterval working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,98 @@
 import { HiMenu } from "react-icons/hi";
 import { IconContext } from "react-icons";
+import { useNavigate } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+import { useGSAP } from "@gsap/react";
 import "../App.css";
 import "./Navbar.css";
-import { useNavigate } from 'react-router-dom';
 
+gsap.registerPlugin(TextPlugin);
 
 const Navbar = (props) => {
   const navigate = useNavigate();
-   const navigateToHome = ()  =>{
-    navigate('/');
-  }
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const containerRef = useRef(null);
+  const tlRef = useRef(null);
 
-  // const onClickHandler = () => {
-  //   console.log('used onclick');
-  // }
+  useGSAP(
+    (context) => {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: 1.3,
+          ease: "ease.in",
+          text: {
+            scramble: 1,
+            delimiter: "",
+            speed: 1,
+          },
+        },
+      });
 
-    // console.log('aboutSectionInview?', props.aboutSectionInView);
+      tl.fromTo(
+        firstNameRef.current,
+        { text: { value: "AZLQIA__" } },
+        { text: { value: "AWSHAF__" } }
+      );
+
+      tl.fromTo(
+        lastNameRef.current,
+        { text: { value: "_________" } },
+        { text: { value: "ISHTIAQUE" }, duration: 2.6, ease: "power2.out" },
+        "-=0.7"
+      );
+
+      tlRef.current = tl;
+    },
+    { scope: containerRef }
+  );
+
+  const restartAnimation = () => {
+    if (tlRef.current) {
+      tlRef.current.restart(); // Restart the timeline
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(restartAnimation, 8500);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const navigateToHome = () => {
+    navigate("/");
+  };
+
   return (
     <nav
+      ref={containerRef}
       className={
-        props.aboutSectionInView || props.invertNavColor || props.invertNavColor2 ? "navOnWhite" : ""
+        props.aboutSectionInView ||
+        props.invertNavColor ||
+        props.invertNavColor2
+          ? "navOnWhite"
+          : ""
       }
-      onMouseEnter={props.textEnterLogo}
+      onMouseEnter={() => {
+        props.textEnterLogo();
+      }}
       onMouseLeave={props.textLeaveLogo}
     >
-      <div className="name-logo" onClick={()=>navigateToHome()}>
-        <h4 className="firstName">AWSHAF__</h4>
-        <h4 className="lastName">ISHTIAQUE</h4>
+      <div
+        className="name-logo"
+        onClick={navigateToHome}
+        onMouseEnter={restartAnimation()}
+      >
+        <h4 ref={firstNameRef} className="firstName">
+          x
+        </h4>
+        <h4 ref={lastNameRef} className="lastName">
+          x
+        </h4>
       </div>
 
       <div className="menu">
@@ -36,7 +102,7 @@ const Navbar = (props) => {
           }}
         >
           <IconContext.Provider value={{ className: "hambergerMenu" }}>
-            <HiMenu></HiMenu>
+            <HiMenu />
           </IconContext.Provider>
         </h4>
       </div>
