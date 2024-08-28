597b5ce1e688bdabe8f8859dcc5e1273dbed4145
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-08-28 00:40:47 -0700

    playing with parallax speed

--- src/screens/AnimationsRoute.js
+++ src/screens/AnimationsRoute.js
@@ -1,35 +1,42 @@
-// import "./Resume.css";
-// import Navbar from "../components/Navbar.js";
-// import { useEffect, useRef, useState, useCallback } from "react";
-// import "../App.css";
-// import { motion } from "framer-motion";
+import React, { useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import './AnimationsRoute.css';
 
-// import { useSelector, useDispatch } from "react-redux";
-// import {
-//   setCursorStyle,
-//   updateCursorPosition,
-// } from "../features/cursor/cursorSlice.js";
+gsap.registerPlugin(ScrollTrigger);
 
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+const AnimationsRoute = () => {
+  const containerRef = useRef(null);
 
-// import returnCursorVariant from "../resources/cursorStyles.js";
-// import ScrollToTop from "../components/ScrollToTop.js";
-// import FullScreenNav from "../components/FullScreenNav.js";
-
-// import Vara from "vara";
-// import { useInView } from "react-intersection-observer";
-import "./AnimationsRoute.css";
-
-const AnimationsRoute = (props) => {
+  useGSAP(() => {
+    const elements = containerRef.current.querySelectorAll("[data-speed]");
+    if (elements.length > 0) {
+      gsap.to(elements, {
+        y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window),
+        ease: "none",
+        scrollTrigger: {
+          start: 0,
+          end: "max",
+          invalidateOnRefresh: true,
+          scrub: 0,
+        },
+      });
+    }
+  }, { scope: containerRef });
 
   return (
-    <div>
+    <div ref={containerRef}>
       <h3>Animations</h3>
+      <div className="box-container">
+        <div className="box green" data-speed="0.25">0.25</div>
+        <div className="box purple" data-speed="0.4">0.4</div>
+        <div className="box orange" data-speed="0">0</div>
+        <div className="box red" data-speed="1">1</div>
+        <div className="box blue" data-speed="0.75">0.75</div>
+      </div>
     </div>
   );
 };
 
-
 export default AnimationsRoute;
-
