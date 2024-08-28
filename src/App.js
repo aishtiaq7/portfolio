2caf201e3f7e18ceeab93938f032693bca370d5f
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-08-28 04:03:54 -0700

    update

--- src/App.js
+++ src/App.js
@@ -228,10 +228,8 @@ function App() {
       });
       t1.from("#intro-slider", {
         xPercent: "-100",
-        // opacity: 0,
         duration: 1.3,
         delay: 1.3,
-        // visibility: 0,
       })
         .from(["#title-1", "#title-2", "#title-3"], {
           opacity: 0,
