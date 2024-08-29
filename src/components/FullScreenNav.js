b7872536fe608586d2e550ae280a6928c9c6d1e3
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-08-29 02:13:16 -0700

    uppdate full screen nav links with buttons, need css change

--- src/components/FullScreenNav.js
+++ src/components/FullScreenNav.js
@@ -16,53 +16,128 @@ const FullScreenNav = (props) => {
   const ulRef = useRef(null); // Ref for the list items
   const tl = useRef(null); // Ref for the timeline
 
+  // const handleNavLinkClick = (e) => {
+  //   e.preventDefault();
+  //   gsap.registerPlugin(ScrollToPlugin);
+
+  //   const currentUrl = location.pathname;
+  //   const targetElement = e.target.innerText.toString().toLowerCase();
+  //   const element = document.getElementById(targetElement);
+
+  //   // console.log("currentUrl => :", currentUrl);
+  //   // console.log("targetElement => :", targetElement);
+  //   // console.log("element => :", element);
+
+  //   const handleNavigation = (path, state = null) => {
+  //     handleCloseButtonClick();
+  //     setTimeout(() => {
+  //       navigate(path, { state });
+  //     }, 750);
+  //   };
+
+  //   const scrollToElement = () => {
+  //     handleCloseButtonClick();
+
+  //     /*
+  //       Higher offset value means go more down the page. 
+  //       Lower (or negative) offset value means scroll up the page.
+  //   */
+  //     const isPhone = window.innerWidth <= 768;
+  //     const offsets = {
+  //       home: 0,
+  //       about: isPhone ? 100 : -54,
+  //       default: isPhone ? 80 : 80, // Default offset for other sections
+  //     };
+
+  //     let offsetYValue;
+
+  //     switch (targetElement) {
+  //       case "home":
+  //         offsetYValue = offsets.home;
+  //         break;
+  //       case "about":
+  //         offsetYValue = offsets.about;
+  //         break;
+  //       default:
+  //         offsetYValue = offsets.default;
+  //         break;
+  //     }
+
+  //     gsap.to(window, {
+  //       duration: targetElement === "contact" ? 2.1 : 1.25,
+  //       scrollTo: {
+  //         y: element,
+  //         offsetY: offsetYValue,
+  //         autoKill: true,
+  //       },
+  //       ease: "expoScale",
+  //     });
+  //   };
+
+  //   if (currentUrl === "/" && targetElement === "contact") {
+  //     console.log("from @home && contact 1st");
+  //     scrollToElement();
+  //   } else if (currentUrl === "/" || element !== null) {
+  //     console.log("from @home2 ,targetElement:=>", targetElement);
+
+  //     if (targetElement === "contact") {
+  //       handleNavigation("/", { targetId: "contact" });
+  //     } else if (targetElement === "learn more") {
+  //       handleNavigation("/learnmore");
+  //     } else {
+  //       scrollToElement();
+  //     }
+  //   } else if (currentUrl === "/learnmore") {
+  //     console.log("from @learnmore", `target:',${targetElement}`);
+  //     switch (targetElement) {
+  //       case "home":
+  //         handleNavigation("/", { targetId: "home" });
+  //         break;
+  //       case "about":
+  //         handleNavigation("/", { targetId: "about" });
+  //         break;
+  //       case "learn more":
+  //         console.log("inside here");
+  //         handleCloseButtonClick();
+  //         handleNavigation("/", { targetId: "contact" });
+  //         break;
+  //       case "contact":
+  //         handleNavigation("/", { targetId: "contact" });
+  //         break;
+  //       default:
+  //         handleNavigation("/");
+  //         break;
+  //     }
+  //   }
+  // };
+
   const handleNavLinkClick = (e) => {
     e.preventDefault();
     gsap.registerPlugin(ScrollToPlugin);
-
+  
     const currentUrl = location.pathname;
     const targetElement = e.target.innerText.toString().toLowerCase();
     const element = document.getElementById(targetElement);
-
-    // console.log("currentUrl => :", currentUrl);
-    // console.log("targetElement => :", targetElement);
-    // console.log("element => :", element);
-
+  
     const handleNavigation = (path, state = null) => {
       handleCloseButtonClick();
       setTimeout(() => {
         navigate(path, { state });
       }, 750);
     };
-
+  
     const scrollToElement = () => {
       handleCloseButtonClick();
-
-    /*
-        Higher offset value means go more down the page. 
-        Lower (or negative) offset value means scroll up the page.
-    */
+  
       const isPhone = window.innerWidth <= 768;
       const offsets = {
         home: 0,
-        about: isPhone ? 100 : -54, 
-        default: isPhone ? 80 : 80, // Default offset for other sections
+        about: isPhone ? 100 : -54,
+        default: isPhone ? 80 : 80,
       };
-
-      let offsetYValue;
-
-      switch (targetElement) {
-        case "home":
-          offsetYValue = offsets.home;
-          break;
-        case "about":
-          offsetYValue = offsets.about;
-          break;
-        default:
-          offsetYValue = offsets.default;
-          break;
-      }
-
+  
+      const offsetYValue = offsets[targetElement] || offsets.default;
+  
       gsap.to(window, {
         duration: targetElement === "contact" ? 2.1 : 1.25,
         scrollTo: {
@@ -73,43 +148,43 @@ const FullScreenNav = (props) => {
         ease: "expoScale",
       });
     };
-
-    if (currentUrl === "/" && targetElement === "contact") {
-      console.log('from @home && contact 1st')
-      scrollToElement();
-    } else if (currentUrl === "/" || element !== null) {
-      console.log('from @home2 ,targetElement:=>', targetElement)
-
-      if (targetElement === 'contact'){
-        handleNavigation("/", { targetId: "contact" });
-      } else if (targetElement === "learn more") {
-        handleNavigation("/learnmore");
+  
+    const navigationActions = {
+      "/": {
+        contact: () => {
+          scrollToElement();
+        },
+        default: () => {
+          if (targetElement === "learn more") {
+            handleNavigation("/learnmore");
+          } else {
+            scrollToElement();
+          }
+        },
+      },
+      "/learnmore": {
+        home: () => handleNavigation("/", { targetId: "home" }),
+        about: () => handleNavigation("/", { targetId: "about" }),
+        contact: () => handleNavigation("/", { targetId: "contact" }),
+        default: () => handleNavigation("/"),
+      },
+    };
+  
+    const executeNavigation = () => {
+      const action =
+        navigationActions[currentUrl]?.[targetElement] ||
+        navigationActions[currentUrl]?.default;
+  
+      if (action) {
+        action();
       } else {
-        scrollToElement();
+        console.error("No navigation action defined for this route.");
       }
-    } else if (currentUrl === "/learnmore") {
-      console.log('from @learnmore', `target:',${targetElement}`)
-      switch (targetElement) {
-        case "home":
-          handleNavigation("/", { targetId: "home" });
-          break;
-        case "about":
-          handleNavigation("/", { targetId: "about" });
-          break;
-        case "learn more":
-          console.log('inside here')
-          handleCloseButtonClick();
-          handleNavigation("/", { targetId: "contact" });
-          break;
-        case "contact":
-          handleNavigation("/", { targetId: "contact" });
-          break;
-        default:
-          handleNavigation("/");
-          break;
-      }
-    }
+    };
+  
+    executeNavigation();
   };
+  
 
   const handleCloseButtonClick = () => {
     tl.current
@@ -164,27 +239,23 @@ const FullScreenNav = (props) => {
 
       <ul ref={ulRef}>
         <li>
-          <a className="fs-nav-links" href="/" onClick={handleNavLinkClick}>
+          <button className="fs-nav-links" onClick={handleNavLinkClick}>
             Home
-          </a>
+          </button>
         </li>
         <li>
-          <a className="fs-nav-links" href="/" onClick={handleNavLinkClick}>
+          <button className="fs-nav-links" onClick={handleNavLinkClick}>
             About
-          </a>
+          </button>
         </li>
         <li className="fs-nav-li">
-          <a className="fs-nav-links" href="/" onClick={handleNavLinkClick}>
+          <button className="fs-nav-links" onClick={handleNavLinkClick}>
             Learn More
-          </a>
+          </button>
         </li>
         <li>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a
+          <button
             className="fs-nav-links"
-            // href={googleDriveResumeLink} 
-            target="_blank"
-            rel="noopener noreferrer"
             onClick={() => {
               handleCloseButtonClick();
               setTimeout(() => {
@@ -193,12 +264,12 @@ const FullScreenNav = (props) => {
             }}
           >
             Resume
-          </a>
+          </button>
         </li>
         <li>
-          <a className="fs-nav-links" href="/" onClick={handleNavLinkClick}>
+          <button className="fs-nav-links" onClick={handleNavLinkClick}>
             Contact
-          </a>
+          </button>
         </li>
       </ul>
     </div>
